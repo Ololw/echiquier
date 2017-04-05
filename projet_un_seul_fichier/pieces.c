724bd01d8d21cac4fd26892cbe72a1a6c7e49164
Ololw <echevet.theo@gmail.com>
2017-04-05 17:00:29 +0200

    Affichage + bugs en moins
    affichage

--- projet_un_seul_fichier/pieces.c
+++ projet_un_seul_fichier/pieces.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef enum { BLANC, NOIR, APACHE } couleur_t;
+typedef enum { K, Q, B, N, R, p, VIDE, AUTRE } piece_t;
+
+
+
 // PARTIE PIECES TMTC
 
 char P[8] = {'K','Q','B','N','R','p',' ','*'};
@@ -40,9 +46,9 @@ case_t case_t_de_pc(piece_t p, couleur_t c)
 	return ca;
 }
 
-void set_case(echiquier_t e, int i, int j, case_t ca)
+void set_case(echiquier_t* e, int i, int j, case_t ca)
 {
-	e.ECHIQUIER[i][j] = ca;
+	e->ECHIQUIER[i][j] = ca;
 	return;
 }
 
@@ -52,37 +58,45 @@ echiquier_t init_echiquier()
 	case_t vide;
 	vide.P = VIDE;
 	vide.C = APACHE;
-	echiquier_t e = { vide };
+	echiquier_t e ;
 
 	for (i = 0; i < 8; i++)
 	{
-		set_case(e,1,i,case_t_de_pc(p,BLANC));
-		set_case(e, 6, i, case_t_de_pc(p, NOIR));
+		for(j=0;j<8;j++)
+		{ 
+			set_case(&e, i, j, vide);
+		}
 	}
 
-	set_case(e, 0, 0, case_t_de_pc(R, BLANC));
-	set_case(e, 7, 0, case_t_de_pc(R, NOIR));
+	for (i = 0; i < 8; i++)
+	{
+		set_case(&e,1,i,case_t_de_pc(p,BLANC));
+		set_case(&e, 6, i, case_t_de_pc(p, NOIR));
+	}
 
-	set_case(e, 0, 1, case_t_de_pc(N, BLANC));
-	set_case(e, 7, 1, case_t_de_pc(N, NOIR));
+	set_case(&e, 0, 0, case_t_de_pc(R, BLANC));
+	set_case(&e, 7, 0, case_t_de_pc(R, NOIR));
 
-	set_case(e, 0, 2, case_t_de_pc(B, BLANC));
-	set_case(e, 7, 2, case_t_de_pc(B, NOIR));
+	set_case(&e, 0, 1, case_t_de_pc(N, BLANC));
+	set_case(&e, 7, 1, case_t_de_pc(N, NOIR));
 
-	set_case(e, 0, 3, case_t_de_pc(Q, BLANC));
-	set_case(e, 7, 3, case_t_de_pc(Q, NOIR));
+	set_case(&e, 0, 2, case_t_de_pc(B, BLANC));
+	set_case(&e, 7, 2, case_t_de_pc(B, NOIR));
 
-	set_case(e, 0, 4, case_t_de_pc(K, BLANC));
-	set_case(e, 7, 4, case_t_de_pc(K, NOIR));
+	set_case(&e, 0, 3, case_t_de_pc(Q, BLANC));
+	set_case(&e, 7, 3, case_t_de_pc(Q, NOIR));
 
-	set_case(e, 0, 5, case_t_de_pc(B, BLANC));
-	set_case(e, 7, 5, case_t_de_pc(B, NOIR));
+	set_case(&e, 0, 4, case_t_de_pc(K, BLANC));
+	set_case(&e, 7, 4, case_t_de_pc(K, NOIR));
 
-	set_case(e, 0, 6, case_t_de_pc(N, BLANC));
-	set_case(e, 7, 6, case_t_de_pc(N, NOIR));
+	set_case(&e, 0, 5, case_t_de_pc(B, BLANC));
+	set_case(&e, 7, 5, case_t_de_pc(B, NOIR));
 
-	set_case(e, 0, 7, case_t_de_pc(R, BLANC));
-	set_case(e, 7, 7, case_t_de_pc(R, NOIR));
+	set_case(&e, 0, 6, case_t_de_pc(N, BLANC));
+	set_case(&e, 7, 6, case_t_de_pc(N, NOIR));
+
+	set_case(&e, 0, 7, case_t_de_pc(R, BLANC));
+	set_case(&e, 7, 7, case_t_de_pc(R, NOIR));
 
 	return e;
 }
@@ -118,12 +132,12 @@ char ligne_de_indice(int i)
 
 int indice_de_colonne(char a)
 {
-    return a-'a';
+    return a-'A';
 }
 
 char colonne_de_indice(int i)
 {
-    return i + 'a';
+    return i + 'A';
 }
 
 int char_ligne_valide(char a)
@@ -167,9 +181,6 @@ typedef struct
 
 echiquier_t jouer_coup(echiquier_t e, char* c)
 {
-	echiquier_t e2;
-	e2 = e;
-
 	case_t ca;
 
 	int c1 = indice_de_colonne(c[0]);
@@ -177,28 +188,29 @@ echiquier_t jouer_coup(echiquier_t e, char* c)
 	int l1 = indice_de_ligne(c[1]);
 	int l2 = indice_de_ligne(c[3]);
 
-	get_case(e, c1, l1, &ca);
-	set_case(e2, c2, l2, ca);
-	set_case(e2,c1 , l1, case_t_de_pc(VIDE, APACHE));
+	get_case(e, l1, c1, &ca);
+	set_case(&e, l2, c2, ca);
+	set_case(&e,l1 , c1, case_t_de_pc(VIDE, APACHE));
 
-	return e2;
+	return e;
 }
 
 int coup_valide(char* c,echiquier_t e)
 {
-	printf("test coup_valide1\n");
 	int v0 = char_colonne_valide(c[0]);
 	int v1 = char_ligne_valide(c[1]);
 	int v2 = char_colonne_valide(c[2]);
 	int v3 = char_ligne_valide(c[3]);
 
+	
+//	printf("\n%i\n%i\n%i\n%i\n", indice_de_colonne(c[0]), indice_de_ligne(c[1]), indice_de_colonne(c[2]), indice_de_ligne(c[3]));
 	case_t cd, cf;
 	get_case(e, indice_de_ligne(c[1]), indice_de_colonne(c[0]), &cd);
 	get_case(e, indice_de_ligne(c[3]), indice_de_colonne(c[2]), &cf);
 
 
-	if (v0 && v1 && v2 && v3 && (c[0] != c[2] || c[1] != c[3]) && cd.P != VIDE && (cf.P == VIDE || (cf.C != cd.C)))
-		return 1;
+	return (v0 && v1 && v2 && v3 && (c[0] != c[2] || c[1] != c[3]) && cd.P != VIDE && (cf.P == VIDE || (cf.C != cd.C)));
+		
 
 
 /*	if (v0&&v1&&v2&&v3)
@@ -209,63 +221,98 @@ int coup_valide(char* c,echiquier_t e)
 		{
 			printf("cond2\n");
 
-			if (cd.P != VIDE && (cf.P == VIDE || (cf.C != cd.C)))
+			if (cd.P != VIDE &&  (cf.C != cd.C))
 			{
 				printf("cond3\n");
 				return 1;
 			}
 
 		}
-	}
-	*/
-
-	else
-		return 0;
+	}*/
+	
 
 
 }
 
-void creer_coup(liste_coup lc, char* c)
+void creer_coup(liste_coup* lc, char* c)
 {
 		 maillon* m = malloc(sizeof(maillon));
-
-		if (lc.debut ==  NULL)
+		 m->suivant = NULL;
+		if (lc->debut ==  NULL)
 		{
 			m->element.couleur = BLANC;
 			m->element.num_coup = 1;
 			echiquier_t e = init_echiquier();
 			if (!coup_valide(c,e))
 			{
-				printf("COUP NON VALIDE\n");
+				printf("COUP NON VALIDE init \n");
 			}
 			else
 			{
 				m->element.echiquier = jouer_coup(e, c);
-				lc.debut = m;
-				lc.fin = m;
+				lc->debut = m;
+				lc->fin = m;
 			}
 		}
 
 		else
 		{
-			if (lc.fin->element.couleur == BLANC)
+			if (lc->fin->element.couleur == BLANC)
 				m->element.couleur = NOIR;
 			else
 				m->element.couleur = BLANC;
 
-			m->element.num_coup = lc.fin->element.num_coup+1;
-			if (!coup_valide(c, lc.fin->element.echiquier))
+			m->element.num_coup = lc->fin->element.num_coup+1;
+			if (!coup_valide(c, lc->fin->element.echiquier))
 			{
 				printf("COUP NON VALIDE\n");
 			}
 			else
 			{
-				m->element.echiquier = jouer_coup(lc.fin->element.echiquier, c);
-				lc.fin->suivant = m;
-				lc.fin = m;
+				m->element.echiquier = jouer_coup(lc->fin->element.echiquier, c);
+				lc->fin->suivant = m;
+				lc->fin = m;
 			}
 		}
 
+	return;
+}
+
+//Partie affichage console
+
+void afficher_echiquier_console(echiquier_t e)
+{
+	int i, j;
+
+	for (i = 7; i >= 0; i--)
+	{
+		printf("\n");
+		for (j = 0; j < 8; j++)
+		{
+			printf("| %c ", lettre_de_piece(piece_t_de_case_t(e.ECHIQUIER[i][j])));
+		}
+		printf("|");
+	}
+
+	printf("\n\n");
+	return;
+}
+
+void afficher_echiquier_liste(liste_coup lc)
+{
+	maillon* m;
+	if (lc.debut != NULL)
+	{
+		m = lc.debut;
+		while (m != NULL)
+		{
+			afficher_echiquier_console(m->element.echiquier);
+			m = m->suivant;
+		}
+	}
+
+	else
+		printf("\ntest lc.debut = NULL\n");
 	return;
 }
 
@@ -274,7 +321,7 @@ void creer_coup(liste_coup lc, char* c)
 int main()
 {
     piece_t p;
-    p = piece_de_lettre('*');
+    p = piece_de_lettre('p');
     printf("%c",lettre_de_piece(p));
 
 
@@ -288,7 +335,16 @@ int main()
 	c[2] = 'A';
 	c[3] = '3';
 
-	creer_coup(lc, c);
+	creer_coup(&lc, c);
+	afficher_echiquier_liste(lc);
+
+
+/*	echiquier_t e;
+	e = init_echiquier();
+	set_case(&e, 7, 5, case_t_de_pc(B, NOIR));
+	afficher_echiquier_console(e); */
+
+
 
     return 0;
 }
