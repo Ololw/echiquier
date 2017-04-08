fd155f0a39929362825b629cb15a551cc707a155
Ololw <echevet.theo@gmail.com>
2017-04-08 17:23:10 +0200

    test pour flo

--- Projet/Application/Version1/the_headers/affichage.h
+++ Projet/Application/Version1/the_headers/affichage.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "echiquier.h"
-//#include "pieces.h"
+#include "fonc_carac.h"
+#include "vt100.h"
 #include "coups.h"
 
-
+void afficher_piece(char c, affiche_func_param_t p);
+void afficher_echiquier_vt(echiquier_t e);
 void afficher_echiquier_console(echiquier_t e);
 void afficher_echiquier_liste(liste_coup lc);
 int jouerPartie_fichier(char* nomFich);
--- Projet/Application/Version2/the_headers/affichage.h
+++ Projet/Application/Version2/the_headers/affichage.h
@@ -5,7 +5,8 @@
 #include "vt100.h"
 #include "coups.h"
 
-
+void afficher_piece(char c, affiche_func_param_t p);
+void afficher_echiquier_vt(echiquier_t e);
 void afficher_echiquier_console(echiquier_t e);
 void afficher_echiquier_liste(liste_coup lc);
 int jouerPartie_fichier(char* nomFich);
